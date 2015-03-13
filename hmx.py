fa6abdd1a58e7a8200f6c3985d1c9d44586cfa72
RalphGorham05 <Rgorham1@gaels.iona.edu>
2015-03-13 09:17:37 -0400

    added threads
    separate thread for each artist

--- hmx.py
+++ hmx.py
@@ -1,18 +1,60 @@
 import mechanize
 from mechanize import Browser
 from bs4 import BeautifulSoup
-
-br = Browser()
-url = 'http://harmonixmusic.com/games/rock-band/request/'
-html = br.open(url)
-br.select_form(nr = 0)
-br.form['title'] = 'Unforgiven'
-br.form['artist'] = 'Metallica'
-response = br.submit()
-
-content = response.read()
-soup = BeautifulSoup(content)
-p = soup.find('p').text
-print p
+import threading
+import time
 
 
+class DMBThread(threading.Thread):
+    def __init__(self, threadID, name, title, artist):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.title = title
+        self.artist = artist
+
+    def run(self):
+        print 'Starting ' + self.name
+        filler(self.title, self.artist)
+        print 'Ending ' + self.name
+
+
+class MayerThread(threading.Thread):
+    def __init__(self, threadID, name, title, artist):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.title = title
+        self.artist = artist
+
+    def run(self):
+        print 'Starting ' + self.name
+        time.sleep(5)
+        filler(self.title, self.artist)
+        print 'Ending ' + self.name
+
+
+def filler(title, artist):
+    br = Browser()
+    url = 'http://harmonixmusic.com/games/rock-band/request/'
+    html = br.open(url)
+    br.select_form(nr = 0)
+    br.form['title'] = title
+    br.form['artist'] = artist
+    response = br.submit()
+
+    content = response.read()
+    soup = BeautifulSoup(content)
+    p = soup.find('p').text
+    print p + ' ' +  title
+
+# Create new threads
+thread1 = DMBThread(1, "DMB",'Grey Street','Dave Matthews Band')
+thread2 = MayerThread(2, "Mayer", 'Gravity','John Mayer')
+
+# Start new Threads
+thread1.start()
+
+thread2.start()
+
+print 'Main thread over'
