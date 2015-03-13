10b72c4c10dfc1355f55321f11263530e0a2cba1
RalphGorham05 <Rgorham1@gaels.iona.edu>
2015-03-13 16:11:24 -0400

    used inheritance to clean it up a bit
    added json to store dicts

--- hmx.py
+++ hmx.py
@@ -7,92 +7,78 @@ import json
 
 
 
-
-class DMBThread(threading.Thread):
-    def __init__(self, threadID, name, title, artist):
+class FormThread(threading.Thread):
+    def __init__(self, name, title, artist):
         threading.Thread.__init__(self)
-        self.threadID = threadID
         self.name = name
         self.title = title
         self.artist = artist
-        self.count = []
-        self.dict = {}
+        self.counter = []
+        self.count = {}
 
-    def run(self):
+    def run(self,delay):
         t = 0
         while t < 10:
             t += 1
             print 'Starting ' + self.name
-            time.sleep(30)
+            time.sleep(delay)
             filler(self.title, self.artist)
             print 'Ending ' + self.name + '\t' + str(t)
-            self.count.append(t)
-            self.dict[self.title] = self.count[len(self.count)-1]
-            #print self.dict
+            self.counter.append(t)
+            self.count[self.title] = self.counter[len(self.counter)-1]
+            #print self.count
 
 
-class MayerThread(threading.Thread):
-    def __init__(self, threadID, name, title, artist):
+
+class DMBThread(FormThread, threading.Thread):
+    def __init__(self, name, title, artist):
+        FormThread.__init__(self, name, title, artist) 
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.title = title
-        self.artist = artist
-        self.count = []
-        self.dict = {}
+        self.counter = []
+        self.count = {}
+        
 
     def run(self):
-        t = 0
-        while t < 10:
-            print 'Starting ' + self.name
-            t += 1
-            time.sleep(40)
-            filler(self.title, self.artist)
-            print 'Ending ' + self.name + '\t' + str(t)
-            self.count.append(t)
-            self.dict[self.title] = self.count[len(self.count)-1]
+        delay = 30
+        FormThread.run(self, delay)
+        
+        
 
-class MetallicaThread(threading.Thread):
-    def __init__(self, threadID, name, title, artist):
+
+class MayerThread(FormThread, threading.Thread):
+    def __init__(self, name, title, artist):
+        FormThread.__init__(self, name, title, artist) 
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.title = title
-        self.artist = artist
-        self.count = []
-        self.dict = {}
+        self.counter = []
+        self.count = {}
 
     def run(self):
-        t = 0
-        while t < 10:
-            print 'Starting ' + self.name
-            t += 1
-            time.sleep(50)
-            filler(self.title, self.artist)
-            print 'Ending ' + self.name + '\t' + str(t)
-            self.count.append(t)
-            self.dict[self.title] = self.count[len(self.count)-1]
+        delay = 40
+        FormThread.run(self, delay)
 
-class MiscThread(threading.Thread):
-    def __init__(self, threadID, name, title, artist):
+
+class MetallicaThread(FormThread, threading.Thread):
+    def __init__(self, name, title, artist):
+        FormThread.__init__(self, name, title, artist) 
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.title = title
-        self.artist = artist
-        self.count = []
-        self.dict = {}
+        self.counter = []
+        self.count = {}
 
     def run(self):
-        t = 0
-        while t < 10:
-            print 'Starting ' + self.name
-            t += 1
-            time.sleep(60)
-            filler(self.title, self.artist)
-            print 'Ending ' + self.name + '\t' + str(t)
-            self.count.append(t)
-            self.dict[self.title] = self.count[len(self.count)-1]
+        delay = 50
+        FormThread.run(self, delay)
+
+
+class MiscThread(FormThread, threading.Thread):
+    def __init__(self, name, title, artist):
+        FormThread.__init__(self, name, title, artist) 
+        threading.Thread.__init__(self)
+        self.counter = []
+        self.count = {}
+
+    def run(self):
+        delay = 60
+        FormThread.run(self, delay)
 
 
 def filler(title, artist):
@@ -107,15 +93,15 @@ def filler(title, artist):
     content = response.read()
     soup = BeautifulSoup(content)
     p = soup.find('p').text
-    print ' '
+    print ''
     print p + ' ' +  title
     print ' '
 
 # Create new threads
-thread1 = DMBThread(1, "DMB",'Dreamgirl','Dave Matthews Band')
-thread2 = MayerThread(2, "Mayer", 'Gravity','John Mayer')
-thread3 = MetallicaThread(3, "Metallica", 'Orion','Metallica')
-thread4 = MiscThread(4, "Miscellaneous", 'Uptown Funk','Mark Ronson Bruno Mars')
+thread1 = DMBThread("DMB",'One Sweet World','Dave Matthews Band')
+thread2 = MayerThread("Mayer", 'Where the Light Is','John Mayer')
+thread3 = MetallicaThread("Metallica", 'Master of Puppets','Metallica')
+thread4 = MiscThread("Miscellaneous", 'amor a la mexicana','thalia')
 
 
 thread1.start()
@@ -128,14 +114,15 @@ thread2.join()
 thread3.join()
 thread4.join()
 
-dmb = thread1.dict
-mayer = thread2.dict
-metallica = thread3.dict
-misc = thread4.dict
+dmb = thread1.count
+mayer = thread2.count
+metallica = thread3.count
+misc = thread4.count
 
 
-with open('my_dict.json', 'w') as f:
+with open('my_dict.json', 'a') as f:
     json.dump(dmb, f)
     json.dump(mayer, f)
     json.dump(metallica, f)
     json.dump(misc, f)
+
